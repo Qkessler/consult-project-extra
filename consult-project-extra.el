2e79a94a4eef915e0fc02b4aa96bb4848da29335
Lou Woell <lou.woell@posteo.de>
2026-01-16 18:21:41 +0100

    fix: rename consult-source-buffer -> consult-source-project-buffer
    See #22.

--- consult-project-extra.el
+++ consult-project-extra.el
@@ -118,9 +118,9 @@ When no project is found and MAY-PROMPT is non-nil ask the user."
 ;; The default `consult-source-project-buffer' has the ?p as narrow key,
 ;; and therefore is in conflict with `consult-project-extra--source-project'.
 (defvar consult-project-extra--source-buffer
-  (let* ((unmodified (if (boundp 'consult-source-buffer)
-                         consult-source-buffer
-                       consult--source-buffer))
+  (let* ((unmodified (if (boundp 'consult-source-project-buffer)
+                         consult-source-project-buffer
+                       consult--source-project-buffer))
          (modified-source (plist-put (plist-put unmodified :hidden nil) :narrow ?b)))
     modified-source))
 
